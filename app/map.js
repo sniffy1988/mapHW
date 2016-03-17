b0c21813d9e84a643574dd38d52f521449a10eff
Sniffy1988 <Sniffy1988@gmail.com>
2016-03-17 14:40:55 +0200

    'Initial'

---
+++ app/map.js
@@ -0,0 +1,37 @@
+angular.module('myApp')
+    .controller('mapCtrl', function ($scope) {
+        var mapC = this;
+        mapC.zoom = 13;
+        mapC.center = {
+            lat: 51.508742,
+            lng: -0.120850
+        };
+        mapC.getCoords = function(){
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                alert("Geolocation is not supported by this browser.");
+            }
+        }
+        function showPosition(position){
+            mapC.center.lat = position.coords.latitude;
+            mapC.center.lng = position.coords.longitude;
+        }
+    })
+    .directive('map', function () {
+        return {
+            restrict: 'E',
+            template: '',
+            scope: {
+                center: '=',
+                zoom: '='
+            },
+            link: function (scope, element, attrs) {
+                var mapProp = {
+                    zoom: scope.zoom,
+                };
+                var map = new google.maps.Map(document.getElementById(attrs.id), mapProp);
+                map.setCenter(new google.maps.LatLng(scope.center.lat,scope.center.lng))
+            }
+        }
+    });
